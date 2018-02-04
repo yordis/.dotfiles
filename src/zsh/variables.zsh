8d09ce140fd23c227772e69b7e3120b4333ccff1
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2018-02-04 07:46:08 -0800

    adding rvm and yarn path

--- src/zsh/variables.zsh
+++ src/zsh/variables.zsh
@@ -1,5 +1,7 @@
 export ZPLUG_HOME=/usr/local/opt/zplug
 export EDITOR="$(which vim)"
 export NOKOGIRI_USE_SYSTEM_LIBRARIES=1
-# export PATH="$PATH:$HOME/.rvm/bin"
+
 export PATH="$PATH:/usr/local/bin:$(go env GOPATH)/bin"
+export PATH="$PATH:$HOME/.config/yarn/global/node_modules/.bin"
+[[ -d "$HOME/.rvm" ]] && export PATH="$PATH:$HOME/.rvm/bin"
