464d894ae6fa807f15471ca8ce25d7a7cdeacb67
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2018-06-11 14:58:29 -0400

    Add NativeScripts autocomplete

--- src/zsh/init.zsh
+++ src/zsh/init.zsh
@@ -17,4 +17,5 @@ source "$DOTFILES_ROOT/src/zsh/functions.zsh"
 source "$DOTFILES_ROOT/src/zsh/keybinding.zsh"
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
-[[ -s "${HOME}/.iterm2_shell_integration.zsh" ]] && source "${HOME}/.iterm2_shell_integration.zsh"
+[[ -s "$HOME/.iterm2_shell_integration.zsh" ]] && source "$HOME/.iterm2_shell_integration.zsh"
+[[ -f "$HOME/.tnsrc" ]] && source "$HOME/.tnsrc"
