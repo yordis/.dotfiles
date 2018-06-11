99a20ba6d0969e63b14d33632cb22475921abf4f
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2018-06-11 15:09:15 -0400

    Add LANG config

--- src/zsh/variables.zsh
+++ src/zsh/variables.zsh
@@ -1,6 +1,7 @@
 export ZPLUG_HOME=/usr/local/opt/zplug
 export EDITOR="$(which vim)"
 export NOKOGIRI_USE_SYSTEM_LIBRARIES=1
+export LANG=en_US.UTF-8
 
 export PATH="$PATH:/usr/local/bin:$(go env GOPATH)/bin"
 export PATH="$PATH:$HOME/.config/yarn/global/node_modules/.bin"
