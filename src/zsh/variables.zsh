059fdb1743b240fc8f8897f2687ba013412da3ae
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2018-06-15 23:25:29 -0400

    Add locals bin to path

--- src/zsh/variables.zsh
+++ src/zsh/variables.zsh
@@ -5,5 +5,7 @@ export LANG=en_US.UTF-8
 
 export PATH="$PATH:/usr/local/bin:$(go env GOPATH)/bin"
 export PATH="$PATH:$HOME/.config/yarn/global/node_modules/.bin"
+export PATH="/usr/local/bin:$PATH"
+
 [[ -d "$HOME/.mix/escripts" ]] && export PATH="$PATH:$HOME/.mix/escripts"
 [[ -d "$HOME/.rvm" ]] && export PATH="$PATH:$HOME/.rvm/bin"
