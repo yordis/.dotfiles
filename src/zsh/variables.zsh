9d2a0287ee83a720362edee68f361b2b720a4509
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2018-06-17 14:55:08 -0400

    Fix PATH

--- src/zsh/variables.zsh
+++ src/zsh/variables.zsh
@@ -3,9 +3,10 @@ export EDITOR="$(which vim)"
 export NOKOGIRI_USE_SYSTEM_LIBRARIES=1
 export LANG=en_US.UTF-8
 
-export PATH="$PATH:/usr/local/bin:$(go env GOPATH)/bin"
-export PATH="$PATH:$HOME/.config/yarn/global/node_modules/.bin"
 export PATH="/usr/local/bin:$PATH"
+export PATH="$PATH:$(go env GOPATH)/bin"
+export PATH="$PATH:$HOME/.config/yarn/global/node_modules/.bin"
+
 
 [[ -d "$HOME/.mix/escripts" ]] && export PATH="$PATH:$HOME/.mix/escripts"
 [[ -d "$HOME/.rvm" ]] && export PATH="$PATH:$HOME/.rvm/bin"
