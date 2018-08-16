8ae346db08878a08322d3386af5c4ccf95f50421
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2018-08-15 19:05:43 -0500

    Add ASDF installer

--- src/installers/3.asdf/installer.sh
+++ src/installers/3.asdf/installer.sh
@@ -2,6 +2,16 @@
 
 message "Installing ASDF"
 
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.5.1
+ASDF_VERSION=v0.5.1
+ASDF_DIR=$HOME/.asdf
 
+[[ -d $ASDF_DIR ]] && rm -rf $ASDF_DIR
 
+git clone https://github.com/asdf-vm/asdf.git $ASDF_DIR --branch $ASDF_VERSION
+
+source "$ASDF_DIR/asdf.sh"
+
+asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
+asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
+asdf plugin-add python https://github.com/tuvistavie/asdf-python.git
+asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
