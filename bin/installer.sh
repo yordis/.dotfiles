b5959b4b6421b8d3189bc88fc8e5a1aaf2a99b35
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2018-08-15 19:05:00 -0500

    Add prompt for run the installer

--- bin/installer.sh
+++ bin/installer.sh
@@ -7,5 +7,14 @@ message "Running Installers"
 INSTALLERS_ROOT="$DOTFILES_ROOT/src/installers"
 
 find $INSTALLERS_ROOT -name installer.sh | while read installer; do
-  source "${installer}"
+  relative_path=$(realpath --relative-to=$INSTALLERS_ROOT $installer)
+  file_name=${relative_path//[1-9]./}
+
+  read -p "Run $file_name? (y/n) " answer </dev/tty
+
+  case $answer in
+      [Yy]* ) source "${installer}";;
+      [Nn]* ) message "${file_name} skipped";;
+      * ) echo "Please answer yes or no.";;
+  esac
 done
