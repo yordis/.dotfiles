a7e3bd391ed89307cc35d0a36b6d557eaa34c939
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2018-08-15 20:30:40 -0500

    Fix asdf installer

--- src/installers/3.asdf/installer.sh
+++ src/installers/3.asdf/installer.sh
@@ -11,10 +11,8 @@ git clone https://github.com/asdf-vm/asdf.git $ASDF_DIR --branch $ASDF_VERSION
 
 source "$ASDF_DIR/asdf.sh"
 
-asdf_install crystal 0.24.1 https://github.com/marciogm/asdf-crystal
-asdf_install erlang 20.2 https://github.com/asdf-vm/asdf-erlang
-asdf_install elixir 1.6.0 https://github.com/asdf-vm/asdf-elixir
-asdf_install go 1.9.3 https://github.com/kennyp/asdf-golang
-asdf_install nodejs 8.9.4 https://github.com/asdf-vm/asdf-nodejs
-asdf_install python 3.6.2 https://github.com/tuvistavie/asdf-python
-asdf_install ruby 2.4.3 https://github.com/asdf-vm/asdf-ruby
+asdf_install erlang 21.0 https://github.com/asdf-vm/asdf-erlang
+asdf_install elixir 1.7.2 https://github.com/asdf-vm/asdf-elixir
+asdf_install nodejs 10.8.0 https://github.com/asdf-vm/asdf-nodejs
+asdf_install python 3.7.0 https://github.com/tuvistavie/asdf-python
+asdf_install ruby 2.5.1 https://github.com/asdf-vm/asdf-ruby
