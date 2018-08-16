bed6c67755fb3d6293cf2dc331917b8729f1d9d6
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2018-08-16 17:37:39 -0500

    Add back, yarn and mix scripts global bins

--- src/zsh/variables.zsh
+++ src/zsh/variables.zsh
@@ -4,5 +4,5 @@ export NOKOGIRI_USE_SYSTEM_LIBRARIES=1
 export LANG=en_US.UTF-8
 
 export PATH="$PATH:/usr/local/bin"
-# export PATH="$PATH:$HOME/.config/yarn/global/node_modules/.bin"
-# [[ -d "$HOME/.mix/escripts" ]] && export PATH="$PATH:$HOME/.mix/escripts"
+export PATH="$PATH:$HOME/.config/yarn/global/node_modules/.bin"
+[[ -d "$HOME/.mix/escripts" ]] && export PATH="$PATH:$HOME/.mix/escripts"
