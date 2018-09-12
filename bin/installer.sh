2b7e37fc1d40d6dadad711f070660cbd9e8a63cf
Yordis Prieto <yordis.prieto@gmail.com>
2018-09-12 07:46:23 -0700

    Reverse realpath removal and fix order of files

--- bin/installer.sh
+++ bin/installer.sh
@@ -6,8 +6,8 @@ message "Running Installers"
 
 INSTALLERS_ROOT="$DOTFILES_ROOT/src/installers"
 
-find $INSTALLERS_ROOT -name installer.sh | while read installer; do
-  relative_path=$installer
+find $INSTALLERS_ROOT -name installer.sh | sort | while read installer; do
+  relative_path=$(realpath --relative-to=$INSTALLERS_ROOT $installer)
   file_name=${relative_path//[1-9]./}
 
   read -p "Run $installer? (y/n) " answer </dev/tty
