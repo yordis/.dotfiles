581369bebd0c516c1df52b7914b7528c061c6cd3
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2018-09-26 20:31:58 -0700

    Add erlang flags

--- src/zsh/variables.zsh
+++ src/zsh/variables.zsh
@@ -2,6 +2,7 @@ export ZPLUG_HOME=/usr/local/opt/zplug
 export EDITOR="$(which vim)"
 export NOKOGIRI_USE_SYSTEM_LIBRARIES=1
 export LANG=en_US.UTF-8
+export ERL_AFLAGS="-kernel shell_history enabled"
 
 export PATH="$PATH:/usr/local/bin"
 export PATH="$PATH:$HOME/.config/yarn/global/node_modules/.bin"
