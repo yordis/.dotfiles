462667a7893d666d3adecf39243bdb2329907a17
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-04-03 11:16:40 -0700

    Remove alias

--- src/zsh/aliases.zsh
+++ src/zsh/aliases.zsh
@@ -26,7 +26,6 @@ alias super-danger-rm="rm -rf"
 alias l="ls -lah"
 alias ll="ls -lh"
 alias ..="cd .."
-alias -="cd -"
 alias finder="open"
 alias show-hidden='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
 alias hide-hidden='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
