72c5aa8020affef4894ee5e0ccc93d15fbb00210
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-08-26 16:23:14 -0700

    Fix libxml2

--- src/zsh/variables.zsh
+++ src/zsh/variables.zsh
@@ -6,8 +6,16 @@ export LC_ALL=$LANG
 
 export ERL_AFLAGS="-kernel shell_history enabled"
 
+export PATH="/usr/local/opt/libxml2/bin:$PATH"
+# For compilers to find libxml2 you may need to set:
+export LDFLAGS="-L/usr/local/opt/libxml2/lib"
+export CPPFLAGS="-I/usr/local/opt/libxml2/include"
+# For pkg-config to find libxml2 you may need to set:
+export PKG_CONFIG_PATH="/usr/local/opt/libxml2/lib/pkgconfig"
+
 export PATH="/usr/local/sbin:$PATH"
 export PATH="/usr/local/bin:$PATH"
 export PATH="$HOME/.config/yarn/global/node_modules/.bin:$PATH"
+
 [[ -d "$HOME/.mix/escripts" ]] && export PATH="$HOME/.mix/escripts:$PATH"
 [[ -d "$HOME/.cargo/bin" ]] && export PATH="$HOME/.cargo/bin:$PATH"
