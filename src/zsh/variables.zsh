2e1ddeae940d464c7c6176acfe89a7575dd1b422
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-10-03 09:15:20 -0700

    Add gopath

--- src/zsh/variables.zsh
+++ src/zsh/variables.zsh
@@ -16,6 +16,7 @@ export PKG_CONFIG_PATH="/usr/local/opt/libxml2/lib/pkgconfig"
 export PATH="/usr/local/sbin:$PATH"
 export PATH="/usr/local/bin:$PATH"
 export PATH="$HOME/.config/yarn/global/node_modules/.bin:$PATH"
+export GOPATH="$HOME/Developer"
 
 [[ -d "$HOME/.mix/escripts" ]] && export PATH="$HOME/.mix/escripts:$PATH"
 [[ -d "$HOME/.cargo/bin" ]] && export PATH="$HOME/.cargo/bin:$PATH"
