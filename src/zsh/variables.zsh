f304a582e1da5a36d83580e188c203a11b880e61
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-10-12 02:33:43 -0700

    Add go bins

--- src/zsh/variables.zsh
+++ src/zsh/variables.zsh
@@ -17,6 +17,7 @@ export PATH="/usr/local/sbin:$PATH"
 export PATH="/usr/local/bin:$PATH"
 export PATH="$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 export GOPATH="$HOME/Developer/GoLang"
+export PATH="$GOPATH/bin:$PATH"
 
 [[ -d "$HOME/.mix/escripts" ]] && export PATH="$HOME/.mix/escripts:$PATH"
 [[ -d "$HOME/.cargo/bin" ]] && export PATH="$HOME/.cargo/bin:$PATH"
