3a41b1e5b157bc2dd897c6a77e8c220499098893
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2020-04-03 16:38:59 -0700

    fix paths

--- roles/zsh/src/path.zsh
+++ roles/zsh/src/path.zsh
@@ -5,6 +5,8 @@ export PATH="$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 export PATH="$HOME/.dotfiles/bin:$PATH"
 export GOPATH="$HOME/Developer/GoLang"
 export PATH="$GOPATH/bin:$PATH"
+export PATH="$DOTFILES_ROOT_ZSH_ROOT/bin/private:$PATH"
+export PATH="$DOTFILES_ROOT_ZSH_ROOT/bin:$PATH"
 [[ -d "$HOME/.mix/escripts" ]] && export PATH="$HOME/.mix/escripts:$PATH"
 [[ -d "$HOME/.cargo/bin" ]] && export PATH="$HOME/.cargo/bin:$PATH"
 
