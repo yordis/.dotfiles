125c9ee19fdc98d579ad56cf5deafda1fa2650c9
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2021-05-22 23:28:27 -0400

    Add theme

--- roles/zsh/src/zplug.zsh
+++ roles/zsh/src/zplug.zsh
@@ -1,7 +1,6 @@
 source "$ZPLUG_HOME/init.zsh"
 
-zplug "mafredri/zsh-async", from:github
-zplug "sindresorhus/pure", use:pure.zsh, from:github, as:theme
+zplug "themes/robbyrussell", from:oh-my-zsh, as:theme
 zplug "zsh-users/zsh-syntax-highlighting", defer:3
 zplug "zsh-users/zsh-autosuggestions"
 zplug "zsh-users/zsh-completions"
@@ -9,7 +8,8 @@ zplug "zsh-users/zsh-completions"
 if ! zplug check --verbose; then
   printf "Install? [y/N]: "
   if read -q; then
-    echo; zplug install
+    echo
+    zplug install
   fi
 fi
 
