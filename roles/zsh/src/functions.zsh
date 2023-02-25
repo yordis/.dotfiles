e6760de265236eb67d08108bfd579cddc1a891c6
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2023-02-24 22:28:02 -0500

    add func

--- roles/zsh/src/functions.zsh
+++ roles/zsh/src/functions.zsh
@@ -101,3 +101,9 @@ function github-clone-repo {
   [ ! -d $org_repo_dir ] && gh repo clone $org_and_repo
   cd $org_repo_dir
 }
+
+function k8s-dashboard-proxy {
+  export POD_NAME=$(kubectl get pods -n default -l "app.kubernetes.io/name=kubernetes-dashboard,app.kubernetes.io/instance=kubernetes-dashboard" -o jsonpath="{.items[0].metadata.name}")
+  echo https://127.0.0.1:8443/
+  kubectl -n default port-forward $POD_NAME 8443:8443
+}
