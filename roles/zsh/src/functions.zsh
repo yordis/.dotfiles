33a78293dafa0034019f09fca1d3e9540cf3aa64
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2023-04-20 01:28:14 -0400

    add k8s-grafana-proxy

--- roles/zsh/src/functions.zsh
+++ roles/zsh/src/functions.zsh
@@ -1,6 +1,6 @@
 function get-app-running-on-port() {
-  port=$1
-  rstring=$2
+  local port=$1
+  local rstring=$2
 
   lsof -wni tcp:"${port}" | grep "${rstring}"
 }
@@ -93,11 +93,11 @@ function make-github-org-dir {
 }
 
 function github-clone-repo {
-  org_and_repo=$1
-  org_name=$(echo $org_and_repo | cut -d'/' -f 1)
-  repo_name=$(echo $org_and_repo | cut -d'/' -f 2)
-  org_dir=$HOME/Developer/github.com/$org_name
-  org_repo_dir=$org_dir/$repo_name
+  local org_and_repo=$1
+  local org_name=$(echo $org_and_repo | cut -d'/' -f 1)
+  local repo_name=$(echo $org_and_repo | cut -d'/' -f 2)
+  local org_dir=$HOME/Developer/github.com/$org_name
+  local org_repo_dir=$org_dir/$repo_name
 
   [ ! -d $org_dir ] && make-github-org-dir $org_name
 
@@ -107,9 +107,9 @@ function github-clone-repo {
 }
 
 function k8s-dashboard-proxy {
-  export POD_NAME=$(kubectl get pods -n default -l "app.kubernetes.io/name=kubernetes-dashboard,app.kubernetes.io/instance=kubernetes-dashboard" -o jsonpath="{.items[0].metadata.name}")
+  local pod_name=$(kubectl get pods -n default -l "app.kubernetes.io/name=kubernetes-dashboard,app.kubernetes.io/instance=kubernetes-dashboard" -o jsonpath="{.items[0].metadata.name}")
   echo https://127.0.0.1:8443/
-  kubectl -n default port-forward $POD_NAME 8443:8443
+  kubectl -n default port-forward $pod_name 8443:8443
 }
 
 function decode64 {
@@ -118,4 +118,11 @@ function decode64 {
 
 function encode64 {
   echo $1 | base64
+}
+
+function k8s-grafana-proxy {
+  local context=$1
+  local namespace=$2
+  local grafana_pod=$(kubectl --context $context get pods -n $namespace -o name | grep grafana)
+  kubectl port-forward --context $context $grafana_pod 8080:3000 -n $namespace
 }
