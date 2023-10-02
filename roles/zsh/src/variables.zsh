fa7ec57505a1e95ab7306a718e1289605601bc1c
Yordis Prieto <yordis.prieto@gmail.com>
2023-10-02 00:57:05 -0400

    add rancher to path
    Signed-off-by: Yordis Prieto <yordis.prieto@gmail.com>

--- roles/zsh/src/variables.zsh
+++ roles/zsh/src/variables.zsh
@@ -7,6 +7,9 @@
 # fi
 
 export JAVA_HOME=$(/usr/libexec/java_home)
+
+export PATH="$DOTFILES_ROOT_ZSH_ROOT/bin/private:$PATH"
+export PATH="$DOTFILES_ROOT_ZSH_ROOT/bin:$PATH"
 export PATH="/usr/local/opt/libxml2/bin:$PATH"
 export PATH="/usr/local/opt/libpq/bin:$PATH"
 export PATH="/usr/local/sbin:$PATH"
@@ -14,8 +17,7 @@ export PATH="/usr/local/bin:$PATH"
 export PATH="$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 export PATH="$HOME/.dotfiles/bin:$PATH"
 export PATH="$GOPATH/bin:$PATH"
-export PATH="$DOTFILES_ROOT_ZSH_ROOT/bin/private:$PATH"
-export PATH="$DOTFILES_ROOT_ZSH_ROOT/bin:$PATH"
+[[ -d "$HOME/.rd/bin" ]] && export PATH="$HOME/.rd/bin:$PATH"
 [[ -d "$HOME/.mix/escripts" ]] && export PATH="$HOME/.mix/escripts:$PATH"
 [[ -d "$HOME/.cargo/bin" ]] && export PATH="$HOME/.cargo/bin:$PATH"
 [[ -d "$HOME/.crc/bin" ]] && export PATH="$HOME/.crc/bin:$PATH"
