7092580aa53bdd22b002f07191b8a649323de964
Yordis Prieto <yordis.prieto@gmail.com>
2024-02-07 21:02:21 -0500

    chore: disable poetry completions

--- roles/zsh/src/completion.zsh
+++ roles/zsh/src/completion.zsh
@@ -18,6 +18,6 @@ if [ $commands[op] ]; then
   compdef _op op
 fi
 
-if [ $commands[poetry] ]; then
-  poetry completions zsh >$DOTFILES_ROOT_ZSH_ROOT/completions/_poetry
-fi
+#if [ $commands[poetry] ]; then
+#  poetry completions zsh >$DOTFILES_ROOT_ZSH_ROOT/completions/_poetry
+#fi
