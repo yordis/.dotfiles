10a98a37867cd3226f2dbfeb2ec4e92721d3ab31
Yordis Prieto <yordis.prieto@gmail.com>
2024-12-28 16:48:46 -0500

    fix: shift select

--- roles/zsh/files/zinit.zsh
+++ roles/zsh/files/zinit.zsh
@@ -2,6 +2,7 @@
 [ ! -d $ZINIT_HOME/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
 source "${ZINIT_HOME}/zinit.zsh"
 
+zinit light jirutka/zsh-shift-select
 zinit light zsh-users/zsh-syntax-highlighting
 zinit light zsh-users/zsh-autosuggestions
 zinit light zsh-users/zsh-completions
